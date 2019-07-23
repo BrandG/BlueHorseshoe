e9640fdeeab175ea0a8b9342a7a7302b91c35e83
Brand Gamblin <brandg@gmail.com>
2019-07-23 16:43:04 -0400

    Moved the dB interaction to follow the command-line interaction. Created functions for getActual and getPrediction. Set the test to run over twenty days, and print out the number of successes and failures.

--- src/testPrediction.py
+++ src/testPrediction.py
@@ -20,21 +20,45 @@ blueHorseshoe = MongoClient().blueHorseshoe
 predictionDB = blueHorseshoe.predictions
 historyDB = blueHorseshoe.history
 
-print "For the date " + startDate
-correctResult = historyDB.find_one({"date" : startDate, "symbol" : symbol}, {"_id":0,"date":1,"high":1,"low":1,"open":1,"close":1,"volume":1})
-print correctResult
-if correctResult == None :
-    print "Invalid Date"
-    sys.exit(0)
+def getActual (sd, s, hDB) :
+    print "For the date " + sd
+    correctResult = hDB.find_one({"date" : sd, "symbol" : s}, {"_id":0,"date":1,"high":1,"low":1,"open":1,"close":1,"volume":1})
+    print correctResult
+    if correctResult == None :
+        print "Invalid Date"
+        return
+    return correctResult
 
-print "\nPrediction from the date " + startDate
-prediction = predictionDB.find_one({"date" : startDate, "symbol" : symbol})
-print prediction
-if prediction == None :
-    print "Invalid Date"
-    sys.exit(0)
 
-if (float(correctResult["high"]) < float(prediction["predictionHigh"])) or (float(correctResult["low"]) > float(prediction["predictionLow"])):
-    print "Failed"
-else:
-    print "Success"
+def getPrediction (sd, s, pDB) :
+    print "\nPrediction from the date " + sd
+    prediction = pDB.find_one({"date" : sd, "symbol" : s})
+    print prediction
+    if prediction == None :
+        print "Invalid Date"
+        return
+    return prediction
+
+
+success = 0
+failure = 0
+for i in range(20) :
+    datetime_object = datetime.strptime(startDate, '%Y-%m-%d')
+    newDate = datetime_object - timedelta(days=i)
+    newDateString = newDate.strftime('%Y-%m-%d')
+    correctResult = getActual(newDateString, symbol, historyDB)
+    prediction = getPrediction(newDateString, symbol, predictionDB)
+
+    if (correctResult != None) and (prediction != None) :
+        if (float(correctResult["high"]) < float(prediction["predictionHigh"])) or (float(correctResult["low"]) > float(prediction["predictionLow"])):
+            success += 1
+            print "Success!!!"
+        else :
+            failure += 1
+            print "failure"
+    print ""
+    print ""
+    print ""
+
+print "Successes = "+str(success)
+print "Failures = "+str(failure)
